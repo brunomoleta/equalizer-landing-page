ee686ee340782b34df0208c09200896a63ae6525
PiriyaLapa <piriyalapa@gmail.com>
2024-02-04 02:12:15 +0700

    updaste and try to deploy

--- src/styles.ts
+++ src/styles.ts
@@ -7,9 +7,13 @@ const GlobalStyles = createGlobalStyle<{
 }>`
     body {
             overflow:hidden;
-        @media screen and (min-width:375px){
+        @media screen and (max-width:375px){
             background-color:green;
         }
+        @media screen and (min-width:375px){
+            background-color:orange;
+        }
+
 
         @media screen and (min-width:768px){
             background-color:yellow
