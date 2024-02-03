a538ed8de8d8d9ba2e2a85c285de53e4c0a32e5e
Piriya <piriyalapa@gmail.com>
2024-02-03 21:18:31 +0700

    try to test to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,40 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import { Component, ReactNode } from 'react';
+import Styled from 'styled-components';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <h1>I'm In</h1>
-//         <h2>Added in</h2>
-        
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+const Font = Styled.h1`
+  font-family:'IBM Plex Sans', sans-serif;
+`
 
-// export default App;
+const Font2 = Styled.h1`
+  font-family:'IBM Plex Sans';
+  font-style:normal;
+  font-weight:400;
+  font-size:40px;
+  line-height:34px;
+`
+const Font3 = Styled.h1`
+  font-family:'IBM Plex Sans' ; 
+  font-styled:normal;
+  font-weight:700;
+  font-size:77px;
+  line-height:88px;
+`
 
-import {Component, ReactNode} from 'react';
+const Font4 = Styled(Font3)`
+font-family:'IBM Plex Sans';
+`
 
-export default class App extends Component{
-  render():ReactNode{
-    return(
+
+export default class App extends Component {
+  render(): ReactNode {
+    return (
       <div>
-        <h1>Hello new project</h1>
+        <Font>Hello new project</Font>
+        <Font2>Hello new project</Font2>
+        <Font3>Lorem Ipsum Dolor Sit</Font3>
+        <Font4>Lorem Ipsum Dolor Sit</Font4>
       </div>
     )
   }
 }
 
-  
