90c290931880bf8d6bbfdd39e327494cc34b6723
Piriya <piriyalapa@gmail.com>
2024-02-03 08:08:21 +0700

    updata to push

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-          <h1>I'm In</h1>
+        <p>I'm In</p>
         <a
           className="App-link"
           href="https://reactjs.org"
