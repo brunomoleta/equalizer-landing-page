2d92bcd3ebdc21ae31e9c9ea6d469bb59bc72f8a
Piriya <piriyalapa@gmail.com>
2024-02-03 20:44:54 +0700

    update and try to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <h1>I'm In</h1>
-        <h2>Added in</h2>
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <h1>I'm In</h1>
+//         <h2>Added in</h2>
         
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import {Component, ReactNode} from 'react';
+
+export default class App extends Component{
+  render():ReactNode{
+    return(
+      <div>
+        <h1>Hello new project</h1>
+      </div>
+    )
+  }
 }
 
-export default App;
+  
