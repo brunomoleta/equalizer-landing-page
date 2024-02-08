b0b98c81a61baf24042754becadd84ae0c73a1a1
PiriyaLapa <piriyalapa@gmail.com>
2024-02-08 23:58:01 +0700

    update and tye to deploye

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -11,6 +11,9 @@ export default class LandingPage extends Component {
         <StyledLandingPage theme={theme} $font={font}>
           <h1>Hellow Landing Page</h1>
           <TestBtn color="green" label='IOS download' size='150px' />
+          <TestBtn color="blue" label='androin' size='150px' />
+          
+          
         </StyledLandingPage>
       </div>
     );
