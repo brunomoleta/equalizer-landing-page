b7a81c2714a336896d250008201afb1de976015f
PiriyaLapa <piriyalapa@gmail.com>
2024-02-09 13:11:24 +0700

    update and try to deloy

--- src/components/Buttons/styles.ts
+++ src/components/Buttons/styles.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 // import { BtnProps } from './TestBtn';
 
 export interface BtnProps {
+  label?: string;
   color?: string;
   size?: string;
   width?: string;
@@ -9,17 +10,32 @@ export interface BtnProps {
 
 export const StyledTestBtn = styled.button<BtnProps>`
   background-color: ${(props) => props.color};
-  // width:200px;
   width${(props) => props.width};
   height: 200px;
   font-size: ${(props) => props.size};
 `;
 
-export const StyledDownloadBtn = styled.button`
-  //
+export const StyledDownloadBtn = styled.button<BtnProps>`
+  border-radius: 12px;
+  background-color: ${(props) => props.label === "Android" ? "#FCFAF9" : "#191826"};
+  color: ${(props) => (props.label === "Android" ? "black" : "#FCFAF9")};
+  border: none;
+  width: 333px;
+  height: 61px;
   display: flex;
-  // align-items: center;
-  // align-items:baseline;
-  align-items:center;
+  align-items: center;
   justify-content: center;
+  font-family: "IBM Plex Sans";
+  font-style: normal;
+  font-weight: 700;
+  text-align: center;
+  letter-spacing: -0.18px;
+
+  :nth-child(2) {
+    margin-left: 5px;
+  }
+  :nth-child(3) {
+    margin-left: 5px;
+    text-transform: capitalize;
+  }
 `;
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,10 +2,12 @@ import { Component } from "react";
 import { theme, font } from "../utilities";
 import { StyledLandingPage } from "./styles";
 import DownloadBtn from "../components/Buttons/DownloadBtn";
+import TestBtn from "../components/Buttons/TestBtn";
 
 export default class LandingPage extends Component {
   onClick = () => {
     console.log('Your cliked button');
+    alert('You cliked Download button');
   }
 
   render() {
@@ -13,8 +15,9 @@ export default class LandingPage extends Component {
       <div>
         <StyledLandingPage theme={theme} $font={font}>
           <h1>Hellow Landing Page</h1>
-          <DownloadBtn onClick={this.onClick} label="android" />
-          <DownloadBtn onClick={this.onClick} label="ios" />
+          {/* <TestBtn onClick={this.onClick} label='test'/> */}
+          <DownloadBtn onClick={this.onClick} label="Android" />
+          <DownloadBtn onClick={this.onClick} label="IOS" />
         </StyledLandingPage>
       </div>
     );
