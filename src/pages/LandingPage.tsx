0d28ceb474e9e4091c70d3b87cd4138d8dad086d
Piriya <piriyalapa@gmail.com>
2024-02-10 08:42:24 +0700

    update

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,9 @@
 import { Component } from "react";
 import { theme, font } from "../utilities";
 import { StyledLandingPage } from "./styles";
-// import Logo from '../components/Logo/Logo';
-import Image from "../components/Img/Image";
-import mobilePng from '../assets/bg-main-mobile.png';
+import Logo from '../components/Logo/Logo';
+// import Image from "../components/Img/Image";
+// import mobilePng from '../assets/bg-main-mobile.png';
 // import DownloadBtn from "../components/Buttons/DownloadBtn";
 
 
@@ -19,8 +19,8 @@ export default class LandingPage extends Component {
       <StyledLandingPage theme={theme} $font={font}>
         {/* <h1>Hello world</h1> */}
         {/* <DownloadBtn label="Android" onClick={this.onClick}/> */}
-        {/* <Logo label="equalizer" /> */}
-        <Image urlImg={mobilePng} />
+        <Logo label="equalizer" />
+        {/* <Image urlImg={mobilePng} /> */}
       </StyledLandingPage>
     );
   }
