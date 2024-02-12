20bacb142fd40e67f1098096cd17befd2997d6cd
PiriyaLapa <piriyalapa@gmail.com>
2024-02-12 20:06:54 +0700

    update and try to deploy

--- src/components/Img/styles.ts
+++ src/components/Img/styles.ts
@@ -1,16 +1,5 @@
 import styled from "styled-components";
 
 export const StyledImage = styled.img`
-  //
   position:absolute;
-  @media screen and (max-width:375px){
-  top:50%;
-  left:50%;
-  transform:translate(-30%,-68%);
-  }
-  @media screen and (min-width:375px){
-  top:50%;
-  left:50%;
-  transform:translate(-30%,-70%);
-  }
 `;
