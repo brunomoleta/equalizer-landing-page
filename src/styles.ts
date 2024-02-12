b9abf5bd51dd9281af3c854bfba11fcc38703eae
PiriyaLapa <piriyalapa@gmail.com>
2024-02-12 16:51:04 +0700

    update and try to deploy

--- src/styles.ts
+++ src/styles.ts
@@ -15,25 +15,25 @@ const GlobalStyles = createGlobalStyle<{
         // Mobile 
         @media screen and (max-width:374px){
             background-color:green;
-            padding: 5% 2%
+            padding: 5% 2%;
         }
 
         // Tablet
         @media screen and (min-width:375px) and (max-width:767px){
             background-color:orange;
-            padding: 5% 2%
+            padding: 5% 2%;
         }
 
         // Labtop
         @media screen and (min-width:768px) and (max-width:1439px){
-            background-color:yellow
-            padding: 5% 2%
+            background-color:yellow;
+            padding: 5% 2%;
         }
 
         // Desktop
         @media screen and (min-width:1440px){
             background-color:red;
-            padding: 5% 2%
+            padding: 5% 2%;
         }
     }
 `;
