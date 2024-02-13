a964081f04c59223b1fea1a9f780ed25290d09fc
Piriya <piriyalapa@gmail.com>
2024-02-13 16:52:04 +0700

    update and try to deploy

--- src/components/Img/styles.ts
+++ src/components/Img/styles.ts
@@ -1,6 +1,25 @@
 import styled from "styled-components";
 
 export const StyledImage = styled.img`
-  position:absolute;
-  top:-30px;
+  position: absolute;
+  top: -30px;
+  /* mobile screen */
+  @media screen and (max-width: 767px) {
+      top:-250px;
+      left:50px;
+    
+
+  }
+
+  /* Tablet screen  */
+  @media screen and (min-width: 768px) and (max-width: 1023px) {
+  }
+
+  /* Laptop screen */
+  @media screen and (min-wdith: 1024px) and (max-width: 1439px) {
+  }
+
+  /* Desktop screen */
+  @media screen and (min-width: 1440px) {
+  }
 `;
