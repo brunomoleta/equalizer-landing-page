6aacabcfeb85c97a34b55f9c70b1459f9e1cd30d
PiriyaLapa <piriyalapa@gmail.com>
2024-02-14 18:00:40 +0700

    update and try to deploy

--- src/components/Vectors/styles.ts
+++ src/components/Vectors/styles.ts
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export const StyledVector = styled.div`
     // Set the position of the component to absolute
     position: absolute;
-    z-index:500;
+    z-index:400;
 
     // Media query for mobile screens (max-width: 767px)
     @media screen and (max-width: 767px) {
@@ -13,18 +13,17 @@ export const StyledVector = styled.div`
 
     // Media query for tablet screens (min-width: 768px) and (max-width: 1023px)
     @media screen and (min-width: 768px) and (max-width: 1023px) {
-        // Set the top and right properties for tablet screens
         top: 0px;
         right: 0px;
     }
 
     // Media query for laptop screens (min-width: 1024px) and (max-width: 1439px)
     @media screen and (min-width: 1024px) and (max-width: 1439px) {
-        // Styles specific to laptop screens can be added here
+        top:0px;
+        right:0px;
     }
 
     // Media query for desktop screens (min-width: 1440px)
     @media screen and (min-width: 1440px) {
-        // Styles specific to desktop screens can be added here
     }
 `;
