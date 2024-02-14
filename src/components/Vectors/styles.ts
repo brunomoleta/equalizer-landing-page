137b91de9881c617c0b76a1fbddfaf1c7d42aa13
PiriyaLapa <piriyalapa@gmail.com>
2024-02-14 15:29:20 +0700

    update and try to deploy

--- src/components/Vectors/styles.ts
+++ src/components/Vectors/styles.ts
@@ -1,24 +1,29 @@
 import styled from "styled-components";
 
+// Define a styled component called StyledVector using styled-components
 export const StyledVector = styled.div`
-    position:absolute;
-    //  Mobile screen
-    @media screen and (max-width:767px){
-        //
-    }
-    // taplet screen
-    @media screen and (min-width:768px) and (max-width:1023px){
-        top:0px;
-        right:0px;
+    // Set the position of the component to absolute
+    position: absolute;
+
+    // Media query for mobile screens (max-width: 767px)
+    @media screen and (max-width: 767px) {
+        // Styles specific to mobile screens can be added here
     }
 
-    // Laptop screen
-    @media screen and (min-width:1024px) and (max-width:1439px)
-        //
+    // Media query for tablet screens (min-width: 768px) and (max-width: 1023px)
+    @media screen and (min-width: 768px) and (max-width: 1023px) {
+        // Set the top and right properties for tablet screens
+        top: 0px;
+        right: 0px;
     }
 
-    // Desktop screen
-    @media screen and (min-width:1440px){
-        ///
+    // Media query for laptop screens (min-width: 1024px) and (max-width: 1439px)
+    @media screen and (min-width: 1024px) and (max-width: 1439px) {
+        // Styles specific to laptop screens can be added here
+    }
+
+    // Media query for desktop screens (min-width: 1440px)
+    @media screen and (min-width: 1440px) {
+        // Styles specific to desktop screens can be added here
     }
 `;
