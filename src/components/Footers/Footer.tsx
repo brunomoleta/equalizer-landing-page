34609f40c87b965ab561177df017704bbf791a4e
Piriya <piriyalapa@gmail.com>
2024-02-18 19:30:31 +0700

    update and try to deploy

---
+++ src/components/Footers/Footer.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {StyledFooter} from './styles'
+
+export default class Footer extends React.Component {
+  public render() {
+    return (
+      <StyledFooter>
+        <h1>Footer component</h1>
+      </StyledFooter>
+    );
+  }
+}
