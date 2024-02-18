e9eac7c3e1c3a5155e3bac4a7ddb77290a45346a
Piriya <piriyalapa@gmail.com>
2024-02-18 18:51:39 +0700

    update to try to deploy

--- src/components/Buttons/styles.ts
+++ src/components/Buttons/styles.ts
@@ -10,14 +10,15 @@ export interface BtnProps {
 
 export const StyledTestBtn = styled.button<BtnProps>`
   background-color: ${(props) => props.color};
-  width:${(props) => props.width};
+  width: ${(props) => props.width};
   height: 200px;
   font-size: ${(props) => props.size};
 `;
 
 export const StyledDownloadBtn = styled.button<BtnProps>`
   border-radius: 12px;
-  background-color: ${(props) => props.label === "Android" ? "#FCFAF9" : "#191826"};
+  background-color: ${(props) =>
+    props.label === "Android" ? "#FCFAF9" : "#191826"};
   color: ${(props) => (props.label === "Android" ? "black" : "#FCFAF9")};
   border: none;
   width: 333px;
@@ -31,9 +32,10 @@ export const StyledDownloadBtn = styled.button<BtnProps>`
   text-align: center;
   letter-spacing: -0.18px;
 
-  &:hover{
-    background-color:${(props) => props.label === 'Android' ? '#FFB964' : '#66E2DC'};
-    transition:0.38s;
+  &:hover {
+    background-color: ${(props) =>
+      props.label === "Android" ? "#FFB964" : "#66E2DC"};
+    transition: 0.38s;
   }
 
   :nth-child(2) {
@@ -42,5 +44,21 @@ export const StyledDownloadBtn = styled.button<BtnProps>`
   :nth-child(3) {
     margin-left: 5px;
     text-transform: capitalize;
+  }
+  /* mobile screen */
+  @media screen and (max-width: 767px) {
+    //
+  }
+
+  /* Tablet screen  */
+  @media screen and (min-width: 768px) and (max-width: 1023px) {
+  }
+
+  /* Laptop screen */
+  @media screen and (min-width: 1024px) and (max-width: 1439px) {
+  }
+
+  /* Desktop screen */
+  @media screen and (min-width: 1440px) {
   }
 `;
