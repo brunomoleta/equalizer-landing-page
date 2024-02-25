b3d0b2e6ede5b136e112cba5d4b7632095e65934
Piriya <piriyalapa@gmail.com>
2024-02-25 17:22:25 +0700

    update and try to deploy

--- src/components/Icons/styles.ts
+++ src/components/Icons/styles.ts
@@ -1,9 +1,28 @@
 import styled from "styled-components";
 
 export const ContainerIcon = styled.div`
-  margin: 20px 10px;
-  width:100px;
-  display:flex;
-  justify-content:space-around;
-  height: auto;
+
+  // Mobile screen size
+  @media screen and (max-width: 767px) {
+    margin: 20px 10px;
+    width: 100px;
+    display: flex;
+    justify-content: space-around;
+    height: auto;
+  }
+
+  // Teblet screen size
+  @media screen and (min-width: 768px) and (max-width: 1023px) {
+    // background-color:green;
+  }
+
+  // Labtop screen size
+  @media screen and (min-width: 1024px) and (max-width: 1439px) {
+    // background-color:blue;
+  }
+
+  // Desktop screen size
+  @media screen and (min-width: 1440px) {
+    // background-color:yellow;
+  }
 `;
