63c33cc479b9d8bed9d1ce2cfe2d71863e01a52d
Piriya <piriyalapa@gmail.com>
2024-02-25 17:17:09 +0700

    update and try to deploy

---
+++ src/components/Icons/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ContainerIcon = styled.div`
+  margin: 20px 10px;
+  width:100px;
+  display:flex;
+  justify-content:space-around;
+  height: auto;
+  border:1px solid red;
+`;
--- src/pages/sections/Footer.tsx
+++ src/pages/sections/Footer.tsx
@@ -2,6 +2,10 @@ import { Component } from 'react'
 import {StyledFooter} from './styles';
 import Logo from '../../components/Logo/Logo';
 import { footerContent } from '../../apis/dataContentApis';import ContentFooter from '../../components/Footers/ContentFooter';
+import { ContainerIcon } from '../../components/Icons/styles';
+import IconFaceBook from '../../components/Icons/IconFaceBook';
+import IconIG from '../../components/Icons/IconIG';
+import IconX from '../../components/Icons/IconX';
 export default class Footer extends Component {
   render() {
     const content= footerContent.content;
@@ -9,6 +13,11 @@ export default class Footer extends Component {
       <StyledFooter>
         <Logo label="Logo" />
         <ContentFooter content={content} />
+        <ContainerIcon>
+          <IconFaceBook />
+          <IconIG />
+          <IconX />
+        </ContainerIcon>
       </StyledFooter>
     )
   }
