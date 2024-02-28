d82a65fc5ccfa80d4ac8514db08578d53113e690
Piriya <piriyalapa@gmail.com>
2024-02-29 06:43:43 +0700

    update and try to deploy

--- src/components/Footers/ContentFooter.tsx
+++ src/components/Footers/ContentFooter.tsx
@@ -1,16 +1,15 @@
 import { Component, ReactNode } from "react";
-import { StyledContentFooter } from "./styles";
+import { StyledContentFooter} from "./styles";
 
 interface IContentFooter {
   content: string;
 }
 class ContentFooter extends Component<IContentFooter> {
-  render(): ReactNode {
-    const { content } = this.props;
-    const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
-    // const emailMatch = content.match(emailRegex);
-
-    return <StyledContentFooter>{content}</StyledContentFooter>;
+  render():ReactNode{
+      const {content} = this.props;
+    return(
+      <StyledContentFooter>{content}</StyledContentFooter>
+    )
   }
 }
 
--- src/components/Footers/styles.ts
+++ src/components/Footers/styles.ts
@@ -14,6 +14,34 @@ export const StyledContentFooter = styled.span`
   font-size: 16px;
   line-height: 26px;
   color: #191826;
+  /* color:red; */
   width: 85%;
   padding: 5% 2%;
+        @media screen and (max-width:767px){
+          padding: 2% 4%;
+        }
+
+        // Tablet
+        @media screen and (min-width:768px) and (max-width:1023px){
+            // background-color:orange;
+            /* padding: 3% 5%; */
+        }
+
+        // Labtop
+        @media screen and (min-width:1024px) and (max-width:1439px){
+            // background-color:yellow;
+            /* padding: 5% 5%; */
+        }
+
+        // Desktop
+        @media screen and (min-width:1440px) and (max-width:1919px){
+            // background-color:red;
+            /* padding:5% 5%; */
+        }
+
+`;
+
+export const Sentence = styled.p``;
+export const Email = styled.span`
+  color:red;
 `;
