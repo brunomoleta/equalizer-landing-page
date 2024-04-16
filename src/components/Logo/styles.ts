5b150e5d3227c1d4cadb9db9dacf42bfbb9b1d8c
Bruno Moleta <brunomoleta@pm.me>
2024-04-16 13:22:06 -0300

    Refactor CSS in Logo and Vector components
    Minor adjustments have been made to the CSS properties in the Logo and Vector components. Remove unnecessary z-indexes

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,10 +1,8 @@
 import styled from "styled-components";
 
-
 export const StyledLogo = styled.div`
-    display:block;
-    padding:5% 3%;
-    z-index:3;
+  display: block;
+  padding: 5% 3%;
 
   /*; mobile screen */
   @media screen and (max-width: 767px) {
@@ -12,19 +10,18 @@ export const StyledLogo = styled.div`
 
   /* Tablet screen  */
   @media screen and (min-width: 768px) and (max-width: 1023px) {
-    padding:3% 3%;
-    grid-area:A;
+    padding: 3% 3%;
+    grid-area: A;
   }
 
   /* Laptop screen */
-  @media screen and (min-width:1024px) and (max-width:1439px){
-    padding-left:5%;
-    grid-area:A;
+  @media screen and (min-width: 1024px) and (max-width: 1439px) {
+    padding-left: 5%;
+    grid-area: A;
   }
 
   /* Desktop screen */
   @media screen and (min-width: 1440px) {
-    padding-left:10%;
+    padding-left: 10%;
   }
-  
 `;
--- src/components/Vectors/styles.ts
+++ src/components/Vectors/styles.ts
@@ -2,36 +2,35 @@ import styled from "styled-components";
 
 // Define a styled component called StyledVector using styled-components
 export const StyledVector = styled.div`
-    // Set the position of the component to absolute
-    position: absolute;
-    z-index:400;
+  // Set the position of the component to absolute
+  position: absolute;
 
-    // Media query for mobile screens (max-width: 767px)
-    @media screen and (max-width: 767px) {
-        // Styles specific to mobile screens can be added here
-    }
+  // Media query for mobile screens (max-width: 767px)
+  @media screen and (max-width: 767px) {
+    margin-block-start: 200px;
+    // Styles specific to mobile screens can be added here
+  }
 
-    // Media query for tablet screens (min-width: 768px) and (max-width: 1023px)
-    @media screen and (min-width: 768px) and (max-width: 1023px) {
-        top: 0px;
-        right: 0px;
-    }
+  // Media query for tablet screens (min-width: 768px) and (max-width: 1023px)
+  @media screen and (min-width: 768px) and (max-width: 1023px) {
+    top: 0px;
+    right: 0px;
+  }
 
-    // Media query for laptop screens (min-width: 1024px) and (max-width: 1439px)
-    @media screen and (min-width: 1024px) and (max-width: 1439px) {
-        top:0px;
-        right:0px;
-    }
+  // Media query for laptop screens (min-width: 1024px) and (max-width: 1439px)
+  @media screen and (min-width: 1024px) and (max-width: 1439px) {
+    top: 0px;
+    right: 0px;
+  }
 
-    // Media query for desktop screens (min-width: 1440px)
-    @media screen and (min-width: 1440px) {
-        top:0px;
-        right:0px;
-    }
+  // Media query for desktop screens (min-width: 1440px)
+  @media screen and (min-width: 1440px) {
+    top: 0px;
+    right: 0px;
+  }
 `;
 
 export const StyledVectorCard = styled.div`
-    display:flex;
-    justify-content:center;
-//
+  display: flex;
+  justify-content: center;
 `;
