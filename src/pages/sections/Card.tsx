231c3ce290647a916fe6209a012629ff29efb281
Bruno Moleta <brunomoleta@pm.me>
2024-04-16 13:12:15 -0300

    Update Card component with main tag
    Also, it has sections within it

--- src/pages/sections/Card.tsx
+++ src/pages/sections/Card.tsx
@@ -1,16 +1,32 @@
 import { Component, ReactNode } from "react";
-import { StyledCard } from "./styles";
+import { StyledBoxHeaderContent, StyledCard } from "./styles";
 import MainCard from "../../components/Cards/MainCard";
 import SubCard from "../../components/Cards/SubCard";
-import { dataSubCard } from "../../apis/dataContentApis";
+import { dataHeaderContent, dataSubCard } from "../../apis/dataContentApis";
+import VectorTablet from "../../components/Vectors/VectorTablet";
 
-export default class Card extends Component {
+interface windowWidthState {
+  windowWidth: number;
+  windowHeight: number;
+}
+
+export default class Card extends Component<windowWidthState> {
   render(): ReactNode {
+    const { title, content } = dataHeaderContent;
+    const { windowWidth } = this.props;
+    const shouldRenderVector = windowWidth >= 768;
     return (
-      <StyledCard>
-        <MainCard />
-        <SubCard {...dataSubCard} />
-      </StyledCard>
+      <main>
+        <StyledBoxHeaderContent>
+          <h1>{title}</h1>
+          <p>{content}</p>
+        </StyledBoxHeaderContent>
+        {shouldRenderVector && <VectorTablet />}
+        <StyledCard>
+          <MainCard />
+          <SubCard {...dataSubCard} />
+        </StyledCard>
+      </main>
     );
   }
 }
